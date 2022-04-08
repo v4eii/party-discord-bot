aa57308c1ae69c63b7e663a13614ce84ff596dcf
vevteev <vevteev@neoflex.ru>
2022-04-08 23:50:24 +0300

    fix soundcloud tracks

--- src/main/kotlin/ussr/party/kabachki/Main.kt
+++ src/main/kotlin/ussr/party/kabachki/Main.kt
@@ -1,8 +1,9 @@
 package ussr.party.kabachki
 
 import discord4j.core.DiscordClient
+import discord4j.core.GatewayDiscordClient
 import discord4j.core.`object`.entity.channel.TextChannel
-import discord4j.core.event.domain.PresenceUpdateEvent
+import discord4j.core.event.domain.Event
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent
 import discord4j.core.event.domain.lifecycle.ReadyEvent
 import discord4j.core.event.domain.message.MessageCreateEvent
@@ -10,6 +11,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.reactive.asFlow
 import kotlinx.coroutines.reactive.awaitFirstOrNull
 import kotlinx.coroutines.reactor.mono
+import reactor.core.publisher.Mono
 import ussr.party.kabachki.command.manager.CommandRegisterManagerImpl
 import ussr.party.kabachki.command.processor.CommandProcessorImpl
 import ussr.party.kabachki.exception.LaunchException
@@ -20,8 +22,10 @@ val configHolder = mutableMapOf<String, Any>()
 
 fun main(args: Array<String>) {
     val botToken = System.getenv("BOT_TOKEN") ?: args.firstOrNull() ?: throw LaunchException("token key is not present")
+    val isDebug = System.getenv("IS_DEBUG")?.toBoolean() ?: false
     val discordClient = DiscordClient.builder(botToken).build()
     configHolder["token"] = botToken
+    configHolder["isDebug"] = isDebug
 
     discordClient.withGateway { gateway ->
         val interactionMono = mono {
@@ -66,36 +70,34 @@ fun main(args: Array<String>) {
             }
         ).and(
             interactionMonoWithResume.onErrorResume { interactionMonoWithResume }
-        ).and(
-            gateway.on(PresenceUpdateEvent::class.java) {
-                mono {
-                    launch {
-                        println("FOR TEST!!!")
-                    }
-                }
-            }
-
-        )
-
+        ).withDebug(gateway, isDebug)
 //            .and(
 //            mono {
-//                gateway.on(MessageCreateEvent::class.java)
-//                    .asFlow()
-//                    .collect { launch { commandProcessorImpl.parseCommand(it) } }
-//            }
-//        )
-//            .and(
-//            mono {
-//                gateway.on(ReadyEvent::class.java)
-//                    .asFlow()
-//                    .collect { launch { println("${it.self.username} alive") } }
-//            }
-//        ).and(
-//            mono {
-//                gateway.on(ChatInputInteractionEvent::class.java)
-//                    .asFlow()
-//                    .collect { launch { if(it.commandName == "ping") it.reply("Pong!") } }
+//                launch {
+//                    while (true) {
+//                        gateway.users.flatMap { it.asMember(Snowflake.of(937689943814328320)) }.flatMap { it.presence }.toStream().toList()
+//                        gateway.guilds.filter { it.name == "Party of Kabachki" }.flatMap { it.channels }.toStream().toList()
+//                        gateway.guilds
+//                            .filter { it.name == "Party of Kabachki" }
+//                            .flatMap { it.channels }
+//                        delay(100000)
+//                    }
+//                }
 //            }
 //        )
     }.block()
 }
+
+fun Mono<*>.withDebug(gateway: GatewayDiscordClient, isDebug: Boolean) =
+    if (isDebug) {
+        this.and(gateway.on(Event::class.java) {
+            mono {
+                launch {
+                    println(it)
+                }
+            }
+        })
+    } else {
+        this
+    }
+
