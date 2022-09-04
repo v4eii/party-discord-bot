f80357808e803db1452c63d84ed87132cd7aa617
vevteev <vevteev@neoflex.ru>
2022-09-04 12:50:52 +0300

    minor update presence reaction

--- src/main/kotlin/ussr/party/kabachki/event/handler/impl/PresenceUpdateEventHandler.kt
+++ src/main/kotlin/ussr/party/kabachki/event/handler/impl/PresenceUpdateEventHandler.kt
@@ -41,7 +41,7 @@ class PresenceUpdateEventHandler : EventHandler<PresenceUpdateEvent> {
         ),
         Triple(
             first = "Yakuza",
-            second = "",
+            second = "%s",
             third = "https://tenor.com/view/%D0%BA%D0%B0%D0%B2%D1%83%D0%BD-%D0%B0%D1%80%D0%B1%D1%83%D0%B7-%D0%B1%D0%B0%D0%B7%D0%B0-gif-25186538"
         ),
         Triple(
@@ -69,7 +69,7 @@ class PresenceUpdateEventHandler : EventHandler<PresenceUpdateEvent> {
         ),
         Triple(
             first = "Spotify",
-            second = "",
+            second = "%s",
             third = "https://tenor.com/view/ddd-gif-22697176"
         )
     )
